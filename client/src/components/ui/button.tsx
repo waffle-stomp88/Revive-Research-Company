e42deaa72239909e0670f6a0c5acf95899e4da63
graysonwillard <35158398-graysonwillard@users.noreply.replit.com>
2025-11-28 05:29:38 +0000

    Restored to '225890d3251245ad47c13b8d0a4eba2715072122'
    Replit-Restored-To: 225890d3251245ad47c13b8d0a4eba2715072122

--- client/src/components/ui/button.tsx
+++ client/src/components/ui/button.tsx
@@ -50,11 +50,11 @@ const Button = React.forwardRef<HTMLButtonElement, ButtonProps>(
     const Comp = asChild ? Slot : "button"
     return (
       <Comp
-        className="inline-flex items-center justify-center whitespace-nowrap text-sm font-medium focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:pointer-events-none disabled:opacity-50 [&_svg]:pointer-events-none [&_svg]:size-4 [&_svg]:shrink-0 hover-elevate active-elevate-2 text-primary-foreground border border-primary-border min-h-10 rounded-md px-8 font-display gap-2 h-12 bg-[#e7fb10]"
+        className={cn(buttonVariants({ variant, size, className }))}
         ref={ref}
         {...props}
       />
-    );
+    )
   },
 )
 Button.displayName = "Button"
